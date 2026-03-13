19da451fcd2efdaf8a5c6fa4b8400c16707bd3e4
Subhadip Jana <149944137+Subhadipjana95@users.noreply.github.com>
2026-03-13 21:37:21 +0530

    dark theme removed.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 
 const fontSans = Geist({
@@ -22,11 +21,10 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      suppressHydrationWarning
       className={cn("antialiased", fontMono.variable, "font-sans", fontSans.variable)}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        {children}
       </body>
     </html>
   )
